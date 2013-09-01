d31e93c963d78d6ee63c2300deab627aace3bee8
jandyjor <jandyjor@gmail.com>
2013-08-31 23:26:52 -0400

    prueba 1
    es una prueba mas profunda

---
+++ BancMobile/Models/Modelos/Cel_prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BancMobile.Models.Modelos
+{
+    public class Cel_prueba
+    {
+        cualquier cosa
+    }
+}
