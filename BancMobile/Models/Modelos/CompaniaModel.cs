7395d7c60e2ddee8f29eb568c82c9bde8ede5070
lagrantorre <lagrantorre@hotmail.com>
2013-08-31 20:50:34 -0400

    Creación del modelo de compania

---
+++ BancMobile/Models/Modelos/CompaniaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BancMobile.Models.Modelos
+{
+    public class CompaniaModel
+    {
+        public int idCompania { get; set; }
+        public string nombreCompania { get; set; }
+    }
+}
