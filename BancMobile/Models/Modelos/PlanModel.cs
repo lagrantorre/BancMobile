56dd444f548758f9560526c4ad08cdbb5c4afe87
Kwannon <paula.riffo.a@gmail.com>
2013-09-01 18:13:55 -0400

    Creación de vista, modelo y controlador de Planes

---
+++ BancMobile/Models/Modelos/PlanModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BancMobile.Models.Modelos
+{
+    public class PlanModel
+    {
+        public int id_plan { get; set; }
+        public int id_compania { get; set; }
+        public String nombre_plan { get; set; }
+        public String descripcion_plan { get; set; }
+        public int valor_plan { get; set; }
+
+
+
+    }
+}
