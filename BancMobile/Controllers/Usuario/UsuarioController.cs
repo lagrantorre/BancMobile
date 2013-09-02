b206604eac0b9db9464cb10e1d2fb1bcab717170
SolangeFune <solange.fune@gmail.com>
2013-09-02 19:46:39 -0400

    Mi primera vez
    Creando controller de Usuario

---
+++ BancMobile/Controllers/Usuario/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BancMobile.Controllers.Usuario
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
