603d52b42489c11c17f90c93f699f96f65421e65
jandyjor <jandyjor@gmail.com>
2013-09-03 01:32:52 -0400

    Modelo, Vista Control de Celular
    Se creo el modelo, la vista y control de Celular

---
+++ BancMobile/Controllers/Celular/CelularController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BancMobile.Controllers.Celular
+{
+    public class CelularController : Controller
+    {
+        //
+        // GET: /Celular/
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BancMobile/Models/Modelos/CelularModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BancMobile.Models.Modelos
+{
+    public class CelularModel
+    {
+        public int id_compania { get; set; }
+        public int numCelular { get; set; }
+        public String tipo { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
